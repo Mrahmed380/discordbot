2294b035d83c15669e7f38a75aef71c8117894e3
imadev82 <69369397+imadev82@users.noreply.github.com>
2020-08-07 21:49:34 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,9 +3,9 @@ exports.run = async(client, msg, args) => {
   const helpembed = new Discord.MessageEmbed()
   .setTitle('Help Menu.')
   .setDescription("Some commands will be shown here. Some will not.")
-  .addField("⚒️ Moderation ⚒️", "**.ban**\n**.kick**\n**.mute**\n**.warn**")
-  .addField("🎱 Fun 🎱",  "**.8ball**\n**.future**\n**.flip**")
-  .addField("💫 Other 💫", "**.userinfo / .whois**\\n**.botinfo**")
+  .addField("Moderation ⚒️", "**.ban** **.kick** **.mute** **.warn**")
+  .addField("Fun 🎱",  "**.8ball** **.future** **.flip**")
+  .addField("Other 💫", "**.userinfo / .whois**\\n**.botinfo**")
   msg.channel.send(helpembed);
  }
 
