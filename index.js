53f9cb67ce38bace2ddfe0b40550684ce54e3b88
imadev82 <69369397+imadev82@users.noreply.github.com>
2020-08-07 23:26:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -38,4 +38,26 @@ client.on('message', async(msg) => {
 
 });
 
+client.on('guildMemberAdd', member => {
+
+   const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+
+  if(!channel) return;
+
+ var welcome = new Discord.MessageEmbed()
+
+  .setAuthor(member.user.tag, member.user.displayAvatarURL())
+
+  .setColor("GREEN")
+
+  .setDescription(`Hey **${member.user.username}**, welcome to **Desire**!')
+
+.addField('\u200B', '\u200B')
+
+ .setFooter(`Member #{member.guild.memberCount}`)
+ channel.send(welcome);
+ 
+
+}
+
 client.login(config.token);
