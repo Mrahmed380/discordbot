118b3c88b7180d104456913e8383d46ca31abc81
imadev82 <69369397+imadev82@users.noreply.github.com>
2020-08-07 21:33:11 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+exports.run = async(client, msg, args) {
+  const helpembed = new Discord.MessageEmbed()
+  .setAuthor("DesireBot Help", message.author.avatarURL)
+  .setDescription("Some commands will be shown here. Some will not.")
+  .addField("⚒️ Moderation ⚒️", "**.ban**\n**.kick**\n**.mute**\n**.warn**")
+  .addField("🎱 Fun 🎱",  "**.8ball**\n**.future**\n**.flip**")
+  .addField("💫 Other 💫", "**.userinfo / .whois**\\n**.botinfo**")
+  msg.channel.sebd(helpembed);
+ }
+
+
